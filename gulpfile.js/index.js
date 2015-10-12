eab6cf890365e853a4f76f67acaa34fc9128b20d
Pete Brousalis <brousapg@gmail.com>
2015-10-12 12:40:48 -0500

    fix broken build

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,3 +1,8 @@
+var gulp = require('gulp');
 var requireDir = require('require-dir');
 
+// Require all tasks in gulpfile.js/tasks
 requireDir('./tasks', { recurse: true })
+
+// Default task runs the dev server
+gulp.task('default', ['server']);
