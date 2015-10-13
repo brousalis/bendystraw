fa3c304d1022851fc1a258ada906ecb6e2027a43
Pete Brousalis <brousapg@gmail.com>
2015-10-13 11:44:01 -0500

    remove protractor

--- gulpfile.js/tasks/tests.js
+++ gulpfile.js/tasks/tests.js
@@ -17,35 +17,6 @@ function runTests (singleRun, done) {
   });
 }
 
-// function runProtractor (done) {
-//   var params = process.argv;
-//   var args = params.length > 3 ? [params[3], params[4]] : [];
-
-//   gulp.src(path.join(config.paths.tests, '/**/*.js'))
-//     .pipe($.protractor.protractor({
-//       configFile: 'protractor.conf.js',
-//       args: args
-//     }))
-//     .on('error', function (err) {
-//       // Make sure failed tests cause gulp to exit non-zero
-//       throw err;
-//     })
-//     .on('end', function () {
-//       // Close browser sync server
-//       browserSync.exit();
-//       done();
-//     });
-// }
-
-// Downloads the selenium webdriver
-// gulp.task('webdriver-update', $.protractor.webdriver_update);
-// gulp.task('webdriver-standalone', $.protractor.webdriver_standalone);
-
-// End to end testing
-// gulp.task('protractor', ['protractor:source']);
-// gulp.task('protractor:source', ['serve:e2e', 'webdriver-update'], runProtractor);
-// gulp.task('protractor:build', ['serve:e2e-build', 'webdriver-update'], runProtractor);
-
 // Karma testing
 gulp.task('test', ['scripts'], function(done) {
   runTests(true, done);
