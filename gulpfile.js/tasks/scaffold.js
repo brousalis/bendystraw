244d0613522034010aac20a57d88f897c7c0076a
Pete Brousalis <brousapg@gmail.com>
2015-12-01 13:18:04 -0600

    add back scaffold

---
+++ gulpfile.js/tasks/scaffold.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp = require('gulp');
+var path = require('path');
+var fs = require('fs');
+var util = require('../util');
+var $ = require('gulp-load-plugins')();
+
+gulp.task('scaffold', function () {
+  function mkdir(path) {
+    try {
+      fs.mkdirSync(path);
+    } catch(e) {
+      if ( e.code != 'EEXIST' ) throw e;
+    }
+  };
+
+  mkdir(path.join(config.paths.src))
+  mkdir(path.join(config.paths.tests))
+  mkdir(path.join(config.paths.src, config.paths.scripts))
+  mkdir(path.join(config.paths.src, config.paths.styles))
+  mkdir(path.join(config.paths.src, config.paths.images))
+
+  fs.writeFile(path.join(config.paths.src, 'index.html'), '')
+  fs.writeFile(path.join(config.paths.src, config.paths.scripts, 'app.coffee'), '')
+  fs.writeFile(path.join(config.paths.src, config.paths.styles, 'app.sass'), '')
+});
+
+module.exports = function(){};
