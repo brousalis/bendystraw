7c9939762305346c867e70f119940135900d3df5
Pete Brousalis <brousapg@gmail.com>
2016-01-13 14:43:52 -0600

    [BUGFIX] remove changed module for styles. is not working properly

--- gulpfile.js/tasks/styles.js
+++ gulpfile.js/tasks/styles.js
@@ -10,7 +10,6 @@ var browserSync = require('browser-sync');
 var wiredep = require('wiredep').stream;
 var sourcemaps = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
-var changed = require('gulp-changed');
 var autoprefixer = require('gulp-autoprefixer');
 
 // Compile the Sass files and autoprefix them
@@ -18,7 +17,6 @@ function styles() {
   var dest = path.join(config.paths.tmp, config.paths.scripts);
 
   return gulp.src([path.join(config.paths.src, config.paths.styles, '/*.{sass,scss}')])
-    .pipe(changed(dest, { extension: '.css' }))
     // .pipe(gulpif(util.fileExists('bower.json'), wiredep({ directory: 'bower_components' })))
     // .on('error', util.errorHandler('wiredep'))
     .pipe(sourcemaps.init())
