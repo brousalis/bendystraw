81da12be8877ebcefcad61a93e9f82b977c0ca4f
Pete Brousalis <brousapg@gmail.com>
2016-01-27 09:38:23 -0600

    [REFACTOR] progress on image task

--- gulpfile.js/tasks/images.js
+++ gulpfile.js/tasks/images.js
@@ -6,6 +6,7 @@ var gulp = require('gulp');
 var filter = require('gulp-filter');
 var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
+var runSequence = require('run-sequence');
 
 // If /images/ is in the path for a bower_component image, strip it out
 // to avoid duplication
@@ -16,42 +17,40 @@ function imageFolder(path) {
 }
 
 // Move all of the images from bower components into the dev images folder
+// This is used by `inject`
 gulp.task('images', function(callback) {
-  var dest = path.join(config.paths.tmp, config.paths.images);
-
   return gulp.src(config.bowerImages)
     .pipe(filter('**/*.{' + config.extensions.images.join(',') + '}'))
     .pipe(rename(imageFolder))
-    .pipe(gulp.dest(dest));
-});
-
-// Optimize all images (including those from bower) and put in the build folder
-gulp.task('images:build', ['images:bower'], function(callback) {
-  var dest = path.join(config.paths.dest, config.paths.images);
-
-  return gulp.src(path.join(config.paths.src, config.paths.images, '**/*'))
-    .pipe(imagemin(config.images))
-    .pipe(gulp.dest(dest));
-});
-
-// Grab images from bower_components, optimize them, then put them in the build folder
-// This is only used in the final build
-gulp.task('images:bower', function(callback) {
-  var dest = path.join(config.paths.dest, config.paths.images);
-
-  return gulp.src(config.bowerImages)
-    .pipe(filter('**/*.{' + config.extensions.images.join(',') + '}'))
-    .pipe(rename(imageFolder))
-    .pipe(gulp.dest(dest));
+    .pipe(gulp.dest(path.join(config.paths.tmp, config.paths.images)));
 });
 
 // Dev task for optimizing images in your source folder
 gulp.task('images:optimize', function(callback) {
-  var dest = path.join(config.paths.src, config.paths.images);
-
   return gulp.src(path.join(config.paths.src, config.paths.images, '**/*'))
     .pipe(imagemin(config.images))
-    .pipe(gulp.dest(dest));
+    .pipe(gulp.dest(path.join(config.paths.src, config.paths.images)));
+});
+
+// Optimize all images (including those from bower) and put in the build folder
+function buildImages(callback) {
+  return gulp.src(path.join(config.paths.src, config.paths.images, '**/*'))
+    .pipe(imagemin(config.images))
+    .pipe(gulp.dest(path.join(config.paths.dest, config.paths.images)));
+}
+
+// Grab images from bower_components, optimize them, then put them in the build folder
+// This is only used in the final build
+function buildBowerImages(callback) {
+  return gulp.src(config.bowerImages)
+    .pipe(filter('**/*.{' + config.extensions.images.join(',') + '}'))
+    .pipe(rename(imageFolder))
+    .pipe(gulp.dest(path.join(config.paths.dest, config.paths.images)));
+}
+
+gulp.task('images:build', function(callback) {
+  buildBowerImages(callback);
+  buildImages(callback);
 });
 
 module.exports = function(){};
