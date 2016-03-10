6980a35b9c3a2b12c53e8bb015dbe8a1e394bd6a
Pete Brousalis <brousapg@gmail.com>
2016-03-10 15:16:49 -0600

    [BUGFIX] fix eco template compilation

--- gulpfile.js/tasks/templates.js
+++ gulpfile.js/tasks/templates.js
@@ -4,7 +4,6 @@ var path = require('path');
 
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
-var jst = require('gulp-jst-concat');
 var eco = require('gulp-eco');
 var browserSync = require('browser-sync').get('server');
 var minifyHtml = require('gulp-minify-html');
@@ -16,12 +15,8 @@ var preprocess = require('gulp-preprocess');
 gulp.task('templates', ['markup'], function(callback) {
   return gulp.src(path.join(config.paths.tmp, config.paths.scripts, '/**/*.html'))
     .pipe(minifyHtml(config.html))
-    .pipe(gulpif(config.angular, angularTemplateCache('templates.js', {
-      module: config.templateModule,
-      root: config.paths.scripts,
-      standalone: true
-    })))
-    .pipe(gulp.dest(path.join(config.paths.tmp, '/templates')))
+    .pipe(gulpif(config.angular, angularTemplateCache('templates.js', { module: config.templateModule, root: config.paths.scripts, standalone: true })))
+    .pipe(gulpif(config.angular, gulp.dest(path.join(config.paths.tmp, '/templates'))))
     .pipe(browserSync.stream());
 });
 
@@ -31,7 +26,7 @@ gulp.task('markup', function(callback) {
 
   return gulp.src(path.join(config.paths.src, config.paths.scripts, '/**/*.{' + config.extensions.templates + '}'))
     .pipe(changed(dest, { extension: '.html' }))
-    .pipe(gulpif(config.eco, eco()))
+    .pipe(gulpif(config.eco, eco({ basePath: path.join(config.paths.src, config.paths.scripts) })))
     .pipe(preprocess({ context: { NODE_ENV: process.env.NODE_ENV } }))
     .pipe(gulp.dest(dest));
 });
