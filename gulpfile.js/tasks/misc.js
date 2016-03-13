9d96b2c454d39a1b96e5da565e50b94bf864ddff
Pete Brousalis <brousapg@gmail.com>
2016-03-13 17:08:43 -0500

    [BUGFIX] add misc task

---
+++ gulpfile.js/tasks/misc.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var path = require('path');
+
+var gulp = require('gulp');
+var filter = require('gulp-filter');
+
+// Used for custom fonts, folders, files in the other folders, etc...
+function misc(dest) {
+  var fileFilter = filter(function (file) {
+    return file.stat.isFile();
+  });
+
+  // This isn't a very good way of doing this :(
+  return gulp.src([
+    path.join(config.paths.src, '/**/*'),
+    path.join('!' + config.paths.src, '/**/*.{' + config.extensions.styles.join(',') + '}'),
+    path.join('!' + config.paths.src, '/**/*.{' + config.extensions.scripts.join(',') + '}'),
+    path.join('!' + config.paths.src, '/**/*.{' + config.extensions.templates.join(',') + '}')
+  ])
+    .pipe(fileFilter)
+    .pipe(gulp.dest(path.join(dest, '/')));
+}
+
+gulp.task('misc', function(callback) {
+  misc(config.paths.tmp);
+  callback();
+});
+
+gulp.task('misc:build', function(callback) {
+  misc(config.paths.dest);
+  callback();
+});
+
+module.exports = misc;
