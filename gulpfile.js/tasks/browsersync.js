548ca0c0467e118176a63ba76becd1a9102efbf1
Pete Brousalis <brousapg@gmail.com>
2016-08-02 15:01:47 -0500

    [FEATURE] add build server

--- gulpfile.js/tasks/browsersync.js
+++ gulpfile.js/tasks/browsersync.js
@@ -50,7 +50,56 @@ function server(callback) {
   callback();
 }
 
+// Development server
+function buildServer(callback) {
+  if (!util.fileExists(config.paths.build)) {
+    util.errorHandler('server:build')(new Error('You need to build the application first. Run `gulp build`'));
+    return;
+  }
+
+  browserSync.init({
+    startPath: '/',
+    server: {
+      baseDir: [config.paths.build],
+      routes: {'/bower_components': 'bower_components'}
+    },
+    notify: config.browserSync.notify,
+    open: config.browserSync.open,
+    port: config.browserSync.port,
+    ghostMode: config.browserSync.ghostMode,
+    logPrefix: function() {
+      return gutil.colors.green('[bendystraw] ');
+    }
+  }, function() {
+    notifier.notify({
+      title: 'bendystraw',
+      message: 'Build server is running on localhost:' + config.browserSync.port,
+      icon: path.join(__dirname, '../lib/logo.png'),
+      sound: true
+    });
+  });
+
+  // Watch the root index file for changes
+  browserSync
+    .watch(path.join(config.paths.src, '*.html'))
+    .on('change', function() {
+      runSequence('inject', reload)
+    });
+
+  callback();
+}
+
+gulp.task('browsersync:build', buildServer);
 gulp.task('browsersync', server);
+
+gulp.task('server:build', function(callback) {
+  util.log('Starting build server from ./' + config.paths.build);
+
+  runSequence(
+    'browsersync:build',
+    callback
+  );
+});
 
 gulp.task('server', function(callback) {
   util.log('Starting server in ' + gutil.colors.yellow(process.env.NODE_ENV) + ' environment');
